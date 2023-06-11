9834b61a0ec63f729ae70f41583667a515866d60
micha <harasim1999@gmail.pl>
2023-06-11 18:42:36 +0200

    FF-2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+             
+        }
+
+         [HttpGet("id")]
+        public async Task<User> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
