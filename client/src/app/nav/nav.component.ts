b2e05e39c95e3abf91dc3f8106435dfdf247e40c
micha <harasim1999@gmail.pl>
2023-06-26 22:22:13 +0200

    FF-9a

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
+  model: any = {}
+  loggedIn = false;
+
+  constructor(private accountServuce: AccountService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  login(){
+    this.accountServuce.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
 }
