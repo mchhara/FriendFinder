3cf4e3f335709b0c80049459e777e1d2a45c1e90
micha <harasim1999@gmail.pl>
2023-06-27 21:30:39 +0200

    FF-9b

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,26 +10,24 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
 
-  model: any = {}
-  loggedIn = false;
+  model: any = {};
+  
 
-  constructor(private accountServuce: AccountService) {}
+  constructor(public accountService: AccountService) {}
 
   ngOnInit(): void {
-    
   }
 
   login(){
-    this.accountServuce.login(this.model).subscribe({
+    this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
