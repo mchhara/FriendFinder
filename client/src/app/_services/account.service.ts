15a6e77dd9e4a65a2573606c64237e5e4c2cec53
micha <harasim1999@gmail.pl>
2023-06-29 16:43:45 +0200

    FF-12

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -23,6 +23,19 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
+        return user;
+      })
+    )
+  }
+
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register',model).pipe(
+      map(user =>{
+          if(user){
+            localStorage.setItem('user', JSON.stringify(user));
+            this.currentUserSource.next(user);
+          }
+          return user;
       })
     )
   }
