cd4c2327cb48ca07a7f5834eb95c248c4727b43d
micha <harasim1999@gmail.pl>
2023-07-03 22:21:24 +0200

    FF-19

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(User user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> GetUserByUsernameAsync(string username);
+    }
+}
