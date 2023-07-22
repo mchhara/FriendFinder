8ad951a3b5c1fa5f3efd1c0c4d1eb3fe35647635
micha <harasim1999@gmail.pl>
2023-07-22 11:56:07 +0200

    FF-56

---
+++ API/Controllers/InvitationsController.cs
@@ -0,0 +1,56 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class InvitationsController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IInvitationsRepository _invitationsRepository;
+        
+        public InvitationsController(IUserRepository userRepository, IInvitationsRepository invitationsRepository)
+        {
+            _invitationsRepository = invitationsRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddInvitation(string username)
+        {
+            var sourceUserId = User.GetUserId();
+            var invidedUser = await _userRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await _invitationsRepository.GetUserWithInvitations(sourceUserId);
+
+            if(invidedUser == null) return NotFound();
+
+            if(sourceUser.Username == username) return BadRequest("You cannot invide to friends yourself");
+
+            var userInvitation = await _invitationsRepository.GetUserInvitation(sourceUserId, invidedUser.Id);
+
+            if(userInvitation != null) return BadRequest("You already invided this user");
+
+            userInvitation = new UserInvitation
+            {
+                SourceUserId = sourceUserId,
+                TargetUserId = invidedUser.Id
+            };
+
+            sourceUser.InvideUsers.Add(userInvitation);
+
+            if(await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Failed to invide user");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InvitationDto>>> GetUserInvitations(string predicate)
+        {
+            var users = await _invitationsRepository.GetUserInvitations(predicate, User.GetUserId());
+
+            return Ok(users);
+        }
+    }
+}
