331f48541200830103ef45b7e80bb4e5cf60c696
micha <harasim1999@gmail.pl>
2023-07-22 12:46:18 +0200

    FF-58

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,7 +22,7 @@ export class NavComponent implements OnInit {
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: () => this.router.navigateByUrl('/main'),
+      next: _ => this.router.navigateByUrl('/members'),
     })
   }
 
